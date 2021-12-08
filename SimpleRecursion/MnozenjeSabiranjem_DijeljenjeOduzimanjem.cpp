40010de487b5c502c29f2956b8d8106a1d2f01eb
Merima <merima.sarancic@edu.fit.ba>
2021-12-08 14:54:54 +0100

    initial commit

---
+++ SimpleRecursion/MnozenjeSabiranjem_DijeljenjeOduzimanjem.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+int mnozenje_sabiranjem(int, int);
+int mnoz_sab_rekurzivno(int, int);
+
+int dijeljenje_oduzimanjem(int, int);
+int dijelj_oduz_rekurzivno(int, int);
+
+int main()
+{
+	int a, b;
+	cout << "Unesite dva broja (MNOZENJE):"<<endl;
+	cin >> a >> b;
+	cout <<"Rezultat(iteracija): "<< mnozenje_sabiranjem(a, b) << endl;
+	cout <<"Rezultat(rekurzija): "<< mnoz_sab_rekurzivno(a, b) << endl;
+	int c, d;
+	cout << "Upisi dva broja (DIJELJENJE):" << endl;
+	cin >> c >> d;
+    if (d != 0)
+    {
+	   cout << "Rezultat(iteracija): " << dijeljenje_oduzimanjem(c,d) << endl;
+	   cout << "Rezultat(rekurzivno): " << dijelj_oduz_rekurzivno(c, d) << endl;
+    }
+    else
+       cout << d <<" je nula!" << endl;
+
+	system("pause>0");
+	return 0;
+}
+
+int mnozenje_sabiranjem(int a, int b)
+{
+	int rezultat = 0;
+	while (a)
+	{
+		rezultat += b;
+		a--;
+	}
+	return rezultat;
+}
+int mnoz_sab_rekurzivno(int a, int b)
+{
+	if (a <= 0)
+		return 0;
+	return b + mnoz_sab_rekurzivno(a - 1, b);
+}
+int dijeljenje_oduzimanjem(int a, int b)
+{
+	int brojac = 0;
+	while (a)
+	{
+		a = a - b;  
+		brojac++;
+	}
+	return brojac;
+}
+int dijelj_oduz_rekurzivno(int a, int b)
+{
+	if (a <= 0)
+		return 0;
+	return dijelj_oduz_rekurzivno(a - b, b) + 1;		
+}
