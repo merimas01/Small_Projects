33a99593f9d0935f49e1ef667bcc17085f56dd18
Merima <merima.sarancic@edu.fit.ba>
2021-12-11 17:20:18 +0100

    updated version

--- Konverzije/Binarni_u_oktalni.cpp
+++ Konverzije/Binarni_u_oktalni.cpp
@@ -2,32 +2,18 @@
 using namespace std;
 
 bool unos_binarnog(int);
+int konverzija_u_oktalni(int);
+
 int main()
 {
-	int binarni, brojac = 0, zbir = 0, ostatak, ost, rezultat=0, i=0;
+	int binarni=0;
 	cout << "Pretvaranje binarnog broja u oktalni." << endl << endl;
 	cout << "Unesi binarni broj:" << endl;
 	cin >> binarni;
 	
 	if (unos_binarnog(binarni))
-	{
-		while (binarni > 0)
-		{
-			ostatak = binarni % 1000;
-			zbir = 0;
-			brojac = 0;        
-			while (ostatak > 0)
-			{
-				ost = ostatak % 10;
-				zbir = zbir + ost * pow(2, brojac);
-				brojac++;
-				ostatak /= 10;
-			}
-			rezultat += zbir * pow(10, i);
-			i++;
-			binarni /= 1000;
-		}
-		cout << "Broj u oktalnom brojnom sistemu je " << rezultat << endl;
+	{		
+		cout << "Broj u oktalnom brojnom sistemu je " << konverzija_u_oktalni(binarni) << endl;
 	}
 	else
 		cout << "Nemoguca konverzija." << endl;
@@ -36,9 +22,30 @@ int main()
 	return 0;
 }
 
+int konverzija_u_oktalni(int binarni) {
+	int brojac = 0, zbir = 0, ostatak=0, ost=0, rezultat = 0, i = 0;
+	while (binarni > 0)
+	{
+		ostatak = binarni % 1000;		   
+		while (ostatak > 0)
+		{
+			ost = ostatak % 10;
+			zbir = zbir + ost * pow(2, brojac);
+			brojac++;
+			ostatak /= 10;
+		}
+		rezultat += zbir * pow(10, i);
+		i++;
+		zbir = 0;
+		brojac = 0;    
+		binarni /= 1000;
+	}
+	return rezultat;
+}
+
 bool unos_binarnog(int bin)
-{	
-	int ost, brojac = 0; 	//1101
+{
+	int ost, brojac = 0; 
 	while (bin > 0)
 	{
 		ost = bin % 10;
